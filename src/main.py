b21caae3a2df62639245c20285b70b5a966a33a6
stefanuslamlo <stefanuslamlo26@gmail.com>
2021-04-04 02:35:33 +0700

    dict

--- src/main.py
+++ src/main.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
 
 import os
-
-def makeInput(text):
+def mergeDict(dict1, dict2):
+   ''' Merge dictionaries and keep values of common keys in list'''
+   dict3 = {**dict1, **dict2}
+   for key, value in dict3.items():
+       if key in dict1 and key in dict2:
+               dict3[key] = [value , dict1[key]]
+   return dict3
+   
+def toDict(text):
     temp = text.split("\n")
+    koordinat =[]
     jumlahNode = int(temp[0])
-    koordinat = temp[1:jumlahNode+1]
-    matriks = temp[jumlahNode+1:]
-    toDict(koordinat,matriks)
+    for i in range(jumlahNode):
+        koordinat += (temp[i+1].split(':'))
 
-def toDict(koordinat, matriks):
-    temp = {"koordinat":[],"edge":[]}
-    newdict = {}
     for i in range(len(koordinat)):
-        temp2 = koordinat[i]
-        newdict[temp2[0]] = temp
-        newdict[temp2[0]]["koordinat"] = ((temp2[1:].replace(":(","")).replace(")","")).split(",")
-        newdict[temp2[0]]["edge"] = makeedge(matriks[i]) 
-        break
-    print(newdict)
-        # newdict[i[0]]["koordinat"] = i[:]
-        # newdict[i[0]]["edge"] = matriks[i]
+        if(i%2==1):
+            koordinat[i]=koordinat[i].split(',')
+
+    dicti = {koordinat[i]:{'koordinat' : koordinat[i + 1]} for i in range(0, len(koordinat), 2)}
+    matriks = temp[jumlahNode+1:]
+
+    for i in range(len(matriks)):
+        matriks[i] = makeedge(matriks[i])
+
+    matriks = {koordinat[i*2]:{'tetangga' : matriks[i]} for i in range(0, len(matriks), 1)}
+    dicti = mergeDict(dicti,matriks)
+    for x, y in dicti.items():
+        print(x, y) 
+
 
 def makeedge(row):
     temp = []
@@ -30,11 +40,11 @@ def makeedge(row):
             temp.append(i+1)
     return temp
 
-PATH = os.path.abspath(os.getcwd())
-filename = "test1.txt"
-test = os.path.join(PATH,"test",filename)
+# PATH = os.path.abspath(os.getcwd())
+# filename = "test1.txt"
+# test = os.path.join(PATH,"test",filename)
+test = "..\\test\\test1.txt"
 with open(test,"r") as f:
     text = f.read()
     f.close()
-# makeInput(text)
-print(makeedge("0,1,1,1,0"))
+toDict(text)
